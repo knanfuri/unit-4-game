c1fec1e2929c173a967a1acbb933323ece27db35
knanfuri <knanfuri@yahoo.com>
2019-03-07 01:55:38 -0600

    HW4

---
+++ assets/javascript/game.js
@@ -0,0 +1,53 @@
+var random_result;
+var lost = 0;
+var win = 0;
+var counter = 0;
+
+// console.log();
+
+var beginGame = function() {
+  $(".crystals").empty();
+  var images = ["", "", "", ""];
+
+  random_result = Math.floor(Math.random() * 69) + 30;
+
+  console.log(random_result);
+
+  $("#result").html("Random Result:" + random_result);
+
+  for (var i = 0; i < 4; i++) {
+    var random = Math.floor(Math.random() * 11) + 1;
+    console.log(random);
+
+    var crystal = $("<div>");
+    crystal.attr({ class: "crystal", "data-random": random });
+    // crystal.html(random);
+    $(".crystals").append(crystal);
+    //   console.log("Game");
+  }
+  $("#counter").html("Score:" + counter);
+};
+
+beginGame();
+
+$(document).on("click", ".crystal", function() {
+  var num = parseInt($(this).attr("data-random"));
+  counter += num;
+
+  $("#counter").html("Score:" + counter);
+  console.log(counter);
+  if (counter > random_result) {
+    Lost++;
+    $("#Lost").html("U LOST!!!!" + lost);
+    counter = 0;
+
+    beginGame();
+
+    // console.log("U LOST");
+  } else if (counter === random_result) {
+    win++;
+    $("#win").html("U WIN!!!!" + win);
+    counter = 0;
+    beginGame();
+  }
+});
